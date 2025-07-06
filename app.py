2a2d11dd6314cebb9a881d4770eaf96e4b64b0a7
Nagaraj Sajjan <nagaraj.devops73@gmail.com>
2025-07-06 18:30:38 +0530

    Trigger full CI/CD

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello DevOps World!"
+    return "Hello Nagaraj DevOps World!"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
