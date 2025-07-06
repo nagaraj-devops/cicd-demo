8c2bd13eca8b7363a295a4c40e5cc738d6372eff
Nagaraj Sajjan <nagaraj.devops73@gmail.com>
2025-07-06 17:52:49 +0530

    added github actions and modified the hello world msg to see pipeline running new

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello CI CD World!"
+    return "Hello DevOps World!"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
