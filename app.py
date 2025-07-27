ce1f12c155bdba54cb8bce9d56baf19673180370
Nagaraj Sajjan <nagaraj@webenza.com>
2025-07-28 05:18:40 +0530

    [2025-07-28 05:18] some updates to check pipeline trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "This is a Flask app running on port 5000!"
+    return "This is a Flask app running on port 5000! On Docker on AWS! on 28th July 2025."
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
