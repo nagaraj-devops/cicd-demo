7bb9a28427dddf0d555046d73d80930029454252
Nagaraj Sajjan <nagaraj@webenza.com>
2025-07-28 04:54:29 +0530

    [2025-07-28 04:54] welcome msg changed from local to see pipeline trigger

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello CI/CD World!"
+    return "This is a Flask app running on port 5000!"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=5000)
